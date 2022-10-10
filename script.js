54dda66ba86a675a399ea71ee76b8254abe681f5
Andre Espinoza <ae95caba@gmail.com>
2022-10-10 09:39:50 -0300

    add buttons to DOM

--- script.js
+++ script.js
@@ -30,3 +30,23 @@ function operate(n1,operator,n2){
             
     }
 }
+
+n=document.getElementById("numbers-container");
+ns= document.createElement("button")
+
+function createNumberButtons(){
+  numbersContainer=document.getElementById("numbers-container");
+  for (let i= 1; i<=10; i++){
+    
+    const button = document.createElement("button");
+    button.setAttribute("class",`number-button`);
+    if(i!==10){
+      button.innerText=`${i}`;
+    }else{button.innerText="0"}
+    
+    numbersContainer.appendChild(button);
+
+  }
+}
+
+createNumberButtons();
